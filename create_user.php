bd641f71bf947dde8007310b34e63e9ef282f838
Rex5433 <149843228+Rex5433@users.noreply.github.com>
2026-04-22 01:36:40 -0500

    added favicon to create user page

--- create_user.php
+++ create_user.php
@@ -51,6 +51,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <title>Create Account</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
     <div class="login-wrapper">
